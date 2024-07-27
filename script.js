3d6faa507e09c522d792e3b4a94762abbe904f65
pjlanger1 <113559711+pjlanger1@users.noreply.github.com>
2024-07-27 14:22:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const searchBar = document.getElementById('search-bar');
     const searchResults = document.getElementById('search-results');
 
-    fetch('https://raw.githubusercontent.com/pjlanger1/pjlanger1.github.io/c1663b28bab1c2201485af8c7d8c507c8637d50d/ref_data/bwref082024_2.json')
+    fetch('https://github.com/pjlanger1/pjlanger1.github.io/blob/4ea6de6d184d6b96d5697547e17181ae29d2c83b/ref_data/bwref082024_2.json')
     .then(response => response.json())
     .then(data => {
         Object.values(data).forEach(location => {
